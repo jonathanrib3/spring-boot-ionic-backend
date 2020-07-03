9b13ffc1436de1c9bb0f3d8a2b7c9a4a7ec3e28d
jonathanrib3 <jonathanrib.3@gmail.com>
2020-07-03 12:31:10 -0300

    Criando operacao de instanciacao

--- src/main/java/com/example/cursomc/CursoSpringApplication.java
+++ src/main/java/com/example/cursomc/CursoSpringApplication.java
@@ -2,14 +2,30 @@
 package com.example.cursomc;
 
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoSpringApplication {
+import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoSpringApplication implements CommandLineRunner {
+	@Autowired
+	CategoriaRepository categoriaRepo;
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"informatica");
+		Categoria cat2 = new Categoria(null, "escritorio");
+		
+		categoriaRepo.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
--- src/main/java/com/example/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/example/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,6 @@
-//endpoint /categorias 
+//endpoint /categorias - controlador rest
 package com.example.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
