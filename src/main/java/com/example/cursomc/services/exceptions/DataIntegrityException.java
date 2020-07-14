a8efe3462d31e31defdce53fc2fc3148253634d8
jonathanrib3 <jonathanrib.3@gmail.com>
2020-07-14 19:33:41 -0300

    Deletando Categoria com DELETE e usando uma excecao nova

---
+++ src/main/java/com/example/cursomc/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,15 @@
+//Exceção personalizada a ser retornada em JSON
+package com.example.cursomc.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DataIntegrityException(String msg) {
+		super(msg);
+	}
+	
+	public DataIntegrityException(String msg, Throwable cause) {
+		super(msg,cause);
+	}
+}
