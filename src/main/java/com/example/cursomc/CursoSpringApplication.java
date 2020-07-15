5958a044a246174e76af4d8838cd115746323b4d
jonathanrib3 <jonathanrib.3@gmail.com>
2020-07-15 02:39:45 -0300

    Paginação com parâmetros adicionais na requisição

--- src/main/java/com/example/cursomc/CursoSpringApplication.java
+++ src/main/java/com/example/cursomc/CursoSpringApplication.java
@@ -63,6 +63,11 @@ public class CursoSpringApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null,"informatica");
 		Categoria cat2 = new Categoria(null, "escritorio");
+		Categoria cat3 = new Categoria(null, "agua coca latao");
+		Categoria cat4 = new Categoria(null, "cozinha");
+		Categoria cat5 = new Categoria(null, "gamer");
+		Categoria cat6 = new Categoria(null, "decoração");
+		Categoria cat7 = new Categoria(null, "jardinagem");
 		Produto p1 = new Produto(null, "computador", 2000);
 		Produto p2 = new Produto(null, "impressora", 800);
 		Produto p3 = new Produto(null, "mouse", 80);
@@ -73,7 +78,7 @@ public class CursoSpringApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepo.saveAll(Arrays.asList(cat1,cat2));
+		categoriaRepo.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7));
 		produtoRepo.saveAll(Arrays.asList(p1,p2,p3));
 		
 		Estado est1 = new Estado(null, "minas gerais");
