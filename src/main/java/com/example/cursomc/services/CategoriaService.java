7f3f57c188afbf766d98ff88baf60ca7b7011392
jonathanrib3 <jonathanrib.3@gmail.com>
2020-07-22 18:39:31 -0300

    Correcoes: update em CategoriaServe e construtor de Pagamento

--- src/main/java/com/example/cursomc/services/CategoriaService.java
+++ src/main/java/com/example/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.domain.Cliente;
 import com.example.cursomc.dto.CategoriaDTO;
 import com.example.cursomc.repositories.CategoriaRepository;
 import com.example.cursomc.services.exceptions.DataIntegrityException;
@@ -33,8 +34,10 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+			Categoria newObj = find(obj.getId());
+			updateData(newObj,obj);
+			return repo.save(newObj);
+		
 	}
 	
 	public void delete(Integer id) {
@@ -58,6 +61,10 @@ public class CategoriaService {
 	
 	public Categoria fromDto(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
 
