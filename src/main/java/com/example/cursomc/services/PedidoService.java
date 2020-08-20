d693882aa69084fcb3fdc3688ee0015a910b8c13
jonathanrib3 <jonathanrib.3@gmail.com>
2020-08-20 20:33:27 -0300

    Implementando toString de Pedido

--- src/main/java/com/example/cursomc/services/PedidoService.java
+++ src/main/java/com/example/cursomc/services/PedidoService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.cursomc.domain.ItemPedido;
 import com.example.cursomc.domain.PagamentoComBoleto;
@@ -27,6 +28,8 @@ public class PedidoService {
 	private ProdutoService produtoService;
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepo;
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -35,10 +38,11 @@ public class PedidoService {
 				"Objeto não encontrado! Id: "+ id + ",Tipo: " + Pedido.class.getName())) ;
 		
 	}
-	
+	@Transactional
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if(obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -50,11 +54,14 @@ public class PedidoService {
 		
 		for(ItemPedido ip: obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		
 		itemPedidoRepo.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
+		
 	}
 }
